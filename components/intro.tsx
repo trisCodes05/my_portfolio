77ae55f8f08f4ff0958e783ec275b89a39332320
codesTris <nainadalmeida@gmail.com>
2023-10-30 12:41:14 +0530

    added resume link

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,7 +97,8 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="Naina_Resume/.pdf"
+          target="_blank"
+          href="https://bit.ly/3PUi2Ti"
           download
         >
           Resume{" "}
