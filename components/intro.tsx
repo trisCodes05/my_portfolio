c3c27cbed433f022fdc0a582ba25a7598839283e
codesTris <nainadalmeida@gmail.com>
2023-11-22 17:03:12 +0530

    resume changed

--- components/intro.tsx
+++ components/intro.tsx
@@ -98,7 +98,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           target="_blank"
-          href="https://bit.ly/3PUi2Ti"
+          href="https://bit.ly/3Ga21nF"
           download
         >
           Resume{" "}
